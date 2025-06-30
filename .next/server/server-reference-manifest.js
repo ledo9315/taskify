8d96901795bd689594bed362ddd32411072941fc
Leonid Domahalskyy <leonid.domahalskyy@icloud.com>
2025-06-30 15:39:55 +0200

    Remove .next build files from repository

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ky1afQxkoNuqjnYGtwLOt1YNrWEIy4BsQDGlckveLmo=\"\n}"
--- .next/static/chunks/src_app_loading_tsx_1cad56d1._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_loading_tsx_9a4bce6e._.js"
-],
-    source: "dynamic"
-});
