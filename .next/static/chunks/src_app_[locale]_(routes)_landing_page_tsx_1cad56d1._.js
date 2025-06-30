1a5a56c2ce3fe19f836df89a997a0f967424d46b
Leonid Domahalskyy <leonid.domahalskyy@icloud.com>
2025-06-30 15:39:55 +0200

    Remove .next build files from repository

--- .next/static/chunks/src_app_[locale]_(routes)_landing_page_tsx_1cad56d1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_294bd977._.js",
-  "static/chunks/node_modules_18df2c2e._.js"
-],
-    source: "dynamic"
-});
