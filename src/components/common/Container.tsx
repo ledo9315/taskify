54606718a7df353d8e9f5638df11531feb29ced8
Leonid Domahalskyy <leonid.domahalskyy@icloud.com>
2025-07-03 14:02:44 +0200

    Refactor: update Container component layout for better responsiveness; remove unnecessary label in TaskInputForm; adjust Dashboard layout for improved structure

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -6,7 +6,7 @@ interface Props {
 export const Container = ({ className, children }: Props) => {
   return (
     <div className={`h-full flex flex-col ${className || ""}`}>
-      <div className="w-full mx-auto flex flex-col h-full animate-fade-in">
+      <div className="w-full lg:w-5/6 xl:w-7/10 2xl:w-6/10 mx-auto flex flex-col h-full animate-fade-in">
         {children}
       </div>
     </div>
